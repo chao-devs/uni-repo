d593daf297d25b772ad382d24174e68cc4deab14
chao <hytline.jp@gmail.com>
2026-01-02 07:34:14 +0900

    UI

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import os,json
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["https://uni-repo.onrender.com"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
