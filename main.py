c53023e70d530f46981de9863fdc68caa018d6e3
chao <hytline.jp@gmail.com>
2026-01-05 17:08:38 +0900

    UI

--- main.py
+++ main.py
@@ -117,7 +117,17 @@ mixed_selected = random.choice(MIXED_RULE)
 
 @app.post("/api/clicklog")
 def data_click():
-    return "ないす"
+    path = Path("click.json")
+    if not os.path.exists(path):
+        with open(path,"w",encoding="utf-8") as f:
+            f.write(json.dumps({"clicks": 0}))
+    
+    with open(path,"r") as f:
+        db = json.loads(f.read())
+    db["clicks"] += 1
+    with open(path,"w") as f:
+        f.write(json.dumps(db,ensure_ascii=False,indent=2))
+    return {"status":"ok","clicks": db["clicks"]}
 
 @app.post("/structure", response_model=StructureResponse)
 def structure(data:Input):
